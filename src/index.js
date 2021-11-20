2ddb7f2409d5602d5fb3ac3c702b64a1f7756313
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2021-11-20 18:27:22 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { ProductsProvider } from './context/products_context'
+import { FilterProvider } from './context/filter_context'
+import { CartProvider } from './context/cart_context'
+import { UserProvider } from './context/user_context'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const { REACT_APP_DOMAIN, REACT_APP_CLIEN_ID } = process.env
+
+ReactDOM.render(
+    <Auth0Provider
+        domain={REACT_APP_DOMAIN}
+        clientId={REACT_APP_CLIEN_ID}
+        redirectUri={window.location.origin}
+        cacheLocation='localstorage'
+    >
+        <UserProvider>
+            <ProductsProvider>
+                <FilterProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </FilterProvider>
+            </ProductsProvider>
+        </UserProvider>
+    </Auth0Provider>
+    , document.getElementById('root'))
---
+++ src/utils/helpers.js
@@ -0,0 +1,16 @@
+export const formatPrice = (num) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(num / 100);
+}
+
+export const getUniqueValues = (data, type) => {
+    let unique = data.map((item) => {
+        return item[type];
+    })
+    if (type === 'colors') {
+        unique = unique.flat();
+    }
+    return ['all', ...new Set(unique)]
+}
